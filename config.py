ba1bdbbfeacddbe6cc3527b8b3667fcc88ec3830
dongyi <juvenpp@gmail.com>
2013-03-14 19:28:00 +0800

    udapte

---
+++ config.py
@@ -0,0 +1,7 @@
+#coding=utf8
+
+SOURCE_GAMELOG_PATH = 'source'
+
+COMPLETED_GAMELOG_PATH = 'completed'
+
+PROCESSING_GAMELOG_PATH = 'processing'
---
+++ processor/profile_attr.py
@@ -0,0 +1,6 @@
+#coding=utf8
+
+source = ''
+
+def run(file=None):
+    pass
