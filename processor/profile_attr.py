b6fd02bee2413c6bd6beb0d5558c6249e628195f
dongyi <juvenpp@gmail.com>
2013-03-15 14:34:09 +0800

    update

--- processor/profile_attr.py
+++ processor/profile_attr.py
@@ -5,13 +5,14 @@ import config
 import common
 
 
-def run(file=None):
-    #  example: xx=xx xx=xx
-    os.system('cp %s %s' % (os.path.join(config.SOURCE_GAMELOG_PATH, file), config.PROCESSING_GAMELOG_PATH))
+def run(filename=None):
+    os.system('cp %s %s' % (os.path.join(config.SOURCE_GAMELOG_PATH, filename), config.PROCESSING_GAMELOG_PATH))
 
     lines = open(file, 'r').readlines()
     for l in lines:
         d = common.txt2dict(l)
-        conn = common.conn
+        sql = """INSERT INTO attr_log(userid, attr, time, old_data, delta_type, delta_data, source) VALUES (%d, %s, %s, %d, %s, %d, %s)"""
+        common.conn.execute(sql,
+                    (d['userid'], d['attr'], d['time'], d['old_data'], d['delta_type'], d['delta_data'], d['source']))
 
-    os.system('mv %s %s' % (os.path.join(config.PROCESSING_GAMELOG_PATH, file), config.COMPLETED_GAMELOG_PATH))
+    os.system('mv %s %s' % (os.path.join(config.PROCESSING_GAMELOG_PATH, filename), config.COMPLETED_GAMELOG_PATH))
