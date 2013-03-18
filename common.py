3ab262c81827fe06338fc9aae887d4bfb6712216
dongyi <juvenpp@gmail.com>
2013-03-18 15:19:22 +0800

    udapte

--- common.py
+++ common.py
@@ -2,7 +2,7 @@
 
 import sys
 sys.path.append('../')
-import pgutils
+import dbutils
 import config
 
 
@@ -10,7 +10,7 @@ def txt2dict(txt):
     return dict([i.split('=') for i in txt.split(' ')])
 
 
-conn = pgutils.Connection(host='127.0.0.1', database=config.DBNAME, user=config.DBUSER, mincached=5)
+conn = dbutils.Connection(host='127.0.0.1', database=config.DBNAME, user=config.DBUSER, password=config.PASSWORD)
 
 if __name__ == '__main__':
     print txt2dict('old_data=100592776 delta_type=0 attr=gold delta_data=6000 userid=1088 source=shortcut_start time=2013-03-14_17:38:03.005773')
