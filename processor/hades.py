0d7b97b25010b54a76cdfc0ad4865675423aeb71
internal_106 <internal_106@vm.com>
2013-03-20 14:08:28 +0800

    udoate

--- processor/hades.py
+++ processor/hades.py
@@ -7,6 +7,7 @@ import common
 
 def run(filename=None):
     #  example: xx=xx xx=xx
+    return
     os.system('cp %s %s' % (os.path.join(config.SOURCE_GAMELOG_PATH, filename), config.PROCESSING_GAMELOG_PATH))
 
     lines = open(filename, 'r').readlines()
--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 #coding=utf8
 
 import os
-import sys
 import config
+import datetime
 from processor.hades import run as hades_run
 from processor.profile_attr import run as profile_attr_run
 from processor.quest import run as quest_run
@@ -19,19 +19,24 @@ def fetch_file():
         return alternate_files.pop()
 
 if __name__ == '__main__':
-    file_path = fetch_file()
-    if not file_path:
-        print 'all log files done'
-        sys.exit(1)
 
-    # 处理未完成的
-    # 处理流程：
-    # 从source里取一个文件，如果是completed里没有的，就放到processing里，进行处理
-    # 处理完成后，移动到completed里
+    while True:
+        file_path = fetch_file()
+        if file_path == None:
+            continue
+        # 处理未完成的
+        # 处理流程：
+        # 从source里取一个文件，如果是completed里没有的，就放到processing里，进行处理
+        # 处理完成后，移动到completed里
+        now = datetime.datetime.now()
+        current_now = '_%d-%d-%d_%d' % (now.year, now.month, now.day, now.hour)
+        if current_now in file_path:
+            continue
 
-    if 'hades' in file_path:
-        hades_run(file_path)
-    elif 'profile_attr' in file_path:
-        profile_attr_run(file_path)
-    elif 'quest' in file_path:
-        quest_run(file_path)
+        if 'hades' in file_path:
+            hades_run(file_path)
+        elif 'profile_attr' in file_path:
+            profile_attr_run(file_path)
+        elif 'quest' in file_path:
+            quest_run(file_path)
+        
