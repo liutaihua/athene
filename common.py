1405f68d511bed79eaf979f8e4bec72643b65d15
internal_106 <internal_106@vm.com>
2013-03-20 19:54:55 +0800

    fix

--- common.py
+++ common.py
@@ -5,15 +5,19 @@ sys.path.append('../')
 
 import config
 import dbutils
+import MySQLdb
 
 def txt2dict(txt):
     try:
         return dict([i.split('=') for i in txt.split(' ') if '=' in i])
     except:
-        print txt
+        print '33333', txt
 
-conn = dbutils.Connection(host='127.0.0.1', database=config.DBNAME, user=config.DBUSER, password=config.PASSWORD)
+#conn = dbutils.Connection(host='localhost', database=config.DBNAME, user=config.DBUSER, password=config.PASSWORD)
+connection = MySQLdb.Connection(host='localhost', db=config.DBNAME, user=config.DBUSER, passwd=config.PASSWORD)
 
+connection.autocommit(True)
+conn = connection.cursor()
 
 if __name__ == '__main__':
     print txt2dict('old_data=100592776 delta_type=0 attr=gold delta_data=6000 userid=1088 source=shortcut_start time=2013-03-14_17:38:03.005773')
