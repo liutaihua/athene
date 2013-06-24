305e7d11ce288b502629bf1c4614c176308dfa39
dongyi <juvenpp@gmail.com>
2013-06-24 17:29:38 +0800

    udapte

--- worker/package.py
+++ worker/package.py
@@ -11,10 +11,10 @@ def run(filename=None):
     lines = open(os.path.join(config.PROCESSING_GAMELOG_PATH, filename), 'r').readlines()
     for l in lines:
         d = common.txt2dict(l)
-        sql = """INSERT INTO package (userid, type, itemid, itemtype, count, source, timestamp, time_tail) \
-            VALUES (%s, %s, %s, '%s', %s, '%s', '%s', %s)""" % (
+        sql = """INSERT INTO package (userid, type, itemid, itemtype, count, source, timestamp) \
+            VALUES (%s, %s, %s, '%s', %s, '%s', '%s')""" % (
             d.get('userid', 0), {'get':1, 'consume':0}[d.get('type', 'get')], d.get('itemid', 0), d.get('itemtype', ''), d.get('count', 1),
-            d.get('source', ''), d.get('time', '').split('.')[0], d.get('time', '').split('.')[-1])
+            d.get('source', ''), d.get('time', '').replace('_', ' '))
         try:
             #print sql
             common.conn.execute(sql)
