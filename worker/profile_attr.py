d3efb56f2dd88720b2a5b12ea0e797e642dddeb8
dongyi <juvenpp@gmail.com>
2013-09-24 15:24:37 +0800

    udapte

--- worker/profile_attr.py
+++ worker/profile_attr.py
@@ -7,7 +7,7 @@ import common
 
 def process_line(l):
     d = common.txt2dict(l)
-    sql = """INSERT INTO attr_log_180 (userid, attr, timestamp, old_data, delta_type, delta_data, source, time_tail) VALUES (%s, '%s', '%s', %s, '%s', %s, '%s', %s)""" % (
+    sql = """INSERT INTO attr_log (userid, attr, timestamp, old_data, delta_type, delta_data, source, time_tail) VALUES (%s, '%s', '%s', %s, '%s', %s, '%s', %s)""" % (
         d.get('userid', 0), d.get('attr', 0), (d.get('time', '').split('.')[0]).replace('_', ' '), d.get('old_data', 0), d.get('delta_type', 0),
         d.get('delta_data', 0), d.get('source', ''), d.get('time', '').split('.')[-1])
     try:
@@ -16,7 +16,6 @@ def process_line(l):
     except Exception, e:
         #pass
         print 'error', e, d.get('time')
-
 
 def run(filename=None):
     os.system('cp %s %s' % (os.path.join(config.SOURCE_GAMELOG_PATH, filename), config.PROCESSING_GAMELOG_PATH))
